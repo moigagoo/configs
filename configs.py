68971179bcc1b9d1fd06401bd1e42a28ac0348c1
Konstantin Molchanov <moigagoo@live.com>
2013-04-11 01:47:40 +0400

    [~]Regex for values improved (0.8)

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ import re
 
 #Regexs for INI header, key-value, and flag item parsing
 header = re.compile('^\s*\[\s*(?P<section>\w+)\s*\]\s*$')
-dict_item = re.compile('^\s*(?P<key>\w+)\s*\=\s*(?P<value>[\S]+)\s*$')
-list_item = re.compile('^\s*(?P<value>[\S]+)\s*$')
+dict_item = re.compile('^\s*(?P<key>\w+)\s*\=\s*(?P<value>.+)\s*$')
+list_item = re.compile('^\s*(?P<value>.+)\s*$')
 
 class Section:
     """INI configuration section
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='configs',
-    version='0.7',
+    version='0.8',
     description='Configuration for humans',
     author='Konstantin Molchanov',
     author_email='moigagoo@myopera.com',
