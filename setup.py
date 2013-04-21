eadbd88d41f87315c9a3de3e970fb56c7de0c273
Konstantin Molchanov <moigagoo@live.com>
2013-04-21 11:53:18 +0400

    [~] Setup file updated.

--- setup.py
+++ setup.py
@@ -7,18 +7,10 @@ setup(
     version=configs.__version__,
     author=configs.__author__,
     description='Configuration for humans',
-    long_description=open('README.rst').read(),
     author_email='moigagoo@myopera.com',
     url='https://bitbucket.org/moigagoo/configs',
     packages=['configs'],
     package_dir={'configs': 'configs'},
     package_data={'configs': ['*.conf']},
     license=configs.__license__,
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        )
 )
