2b91c6ad0d1f0fc04b66d29447121d6f35aa3c87
Konstantin Molchanov <moigagoo@myopera.com>
2013-05-04 00:21:39 +0400

    API: the load function description updated.

--- configs/api.py
+++ configs/api.py
@@ -9,7 +9,7 @@ This module implements the configs API.
 from .config import Config
 
 def load(config_file, fallback_file=None):
-    """Constructs are returns a :class:`Config <Config>` instance.
+    """Constructs and returns a :class:`Config <Config>` instance.
 
     :param config_file: configuration file to be parsed
     :param fallback_file: (optional) fallback configuration file with default values to be used if missing in the ``config_file``
