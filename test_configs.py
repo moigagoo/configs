352377519a5755a4136642a23151ce7f0d8d5334
Konstantin Molchanov <moigagoo@live.com>
2014-07-05 16:25:37 +0400

    Tests updated.

--- test_configs.py
+++ test_configs.py
@@ -15,26 +15,11 @@ class TestApi(unittest.TestCase):
 
         self.assertIsInstance(api.load(self.sample_config_filename), Config)
 
-    def test_load_with_fallback(self):
-        """Checks if a valid config file with a valid fallback config file is parsed correctly"""
-
-        self.assertIsInstance(api.load(self.sample_config_filename, self.fallback_config_filename), Config)
-
-        self.assertEqual(api.load(self.sample_config_filename, self.fallback_config_filename)['general']['foo'], 'baz')
-
-        self.assertEqual(api.load(self.sample_config_filename, self.fallback_config_filename)['general']['spam'], 'eggs')
-
     def test_load_missing_file(self):
         """Checks if the correct exception in raised when a missing config file is attempted to parse"""
 
         with self.assertRaises(FileNotFoundError):
             api.load(self.missing_config_filename)
-
-    def test_load_missing_fallback_file(self):
-        """Checks if the correct exception in raised when a config file with a missing fallback config file is attempted to parse"""
-
-        with self.assertRaises(FileNotFoundError):
-            api.load(self.sample_config_filename, self.missing_config_filename)
 
     def test_load_check_types(self):
         """Checks automatic float, integer, and boolean value conversion"""
@@ -42,11 +27,6 @@ class TestApi(unittest.TestCase):
         self.assertIsInstance(api.load(self.sample_config_filename)['list_section'][0], float)
         self.assertIsInstance(api.load(self.sample_config_filename)['list_section'][1], int)
         self.assertIsInstance(api.load(self.sample_config_filename)['mixed']['boolean'], bool)
-
-    def test_load_with_defaults(self):
-        """Checks if a valid config file with valid defaults is parsed correctly"""
-
-        self.assertIsInstance(api.load(self.sample_config_filename, defaults={'default_key': 'default_value'}), Config)
 
 
 if __name__ == '__main__':
